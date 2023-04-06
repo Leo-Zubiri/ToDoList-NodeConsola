73ea1470ba57963c33c96e386481b1b14a4dce35
Leo-Zubiri <leo.zubiri@outlook.com>
2023-04-05 21:28:06 -0600

    add Model Tarea|Tareas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import colors from 'colors';
 import { inquirerMenu, pausa} from './helpers/inquirer.js'
+import Tarea from './models/tarea.js';
+import Tareas from './models/tareas.js';
 //const {mostrarMenu,pausa} = require('./helpers/mensajes');
 
 console.clear();
@@ -9,9 +11,16 @@ const main = async() => {
     let opc = '';
 
     do {
-        //opc = await mostrarMenu();
+        opc = await mostrarMenu();
         opc = await inquirerMenu();
         console.log(opc);
+
+        // const tarea = new Tarea('Nueva Tarea');
+        // const tareas = new Tareas();
+        // tareas[tarea.id] = tarea;
+
+        // console.log(tarea);
+        // console.log(tareas);
 
         if(opc !== '0') await pausa();
 
