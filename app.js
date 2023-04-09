f9ec831a229ed256bf1eff581d120755e333e80f
Leo-Zubiri <leo.zubiri@outlook.com>
2023-04-08 23:42:25 -0600

    Add Listar Tareas (obj to arr)

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const main = async() => {
                 tareas.crearTarea(desc)
                 break;
             case '2':
-                console.log(tareas._listado);
+                console.log(tareas.ListadoArr);
                 break;
             case '3':
                 break;
